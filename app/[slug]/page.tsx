d76748c68e3636daa5b17d420f05b4a603bad779
alexRicc2 <alex.65.santanna@gmail.com>
2023-09-09 23:43:00 -0300

    blog pages in nextjs13

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { getAllPostsWithSlug } from "@/lib/queries/Post/getAllPostsSlug"
 import { getPostBySlug } from "@/lib/queries/Post/getPostBySlug"
+import {notFound} from 'next/navigation'
 
 export async function generateStaticParams() {
   const posts = await getAllPostsWithSlug()
@@ -7,8 +8,21 @@ export async function generateStaticParams() {
   return posts.map((post) => { return { slug: post.slug } })
 }
 
+// export async function generateMetadata(params: any){
+//   const slug = params?.slug
+//   const post = await getPostBySlug(slug)
+//   if(!post){
+//     return {}
+//   }
+//   return post
+// }
+
 export default async function Page({params}: {params: {slug: string}}){
   const data = await getPostBySlug(params?.slug)
+  
+  if(!data.post){
+    return notFound()
+  }
 
   return (
     <div>
